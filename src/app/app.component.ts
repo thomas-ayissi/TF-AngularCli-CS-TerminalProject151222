3f7f174acb7fbd22c9fe424995622ffdddafc2c5
Thomas Ayissi <thomas8sea@gmail.com>
2022-12-15 21:40:28 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'TF-Angular-Store-Management-Project-0414122';
+}
