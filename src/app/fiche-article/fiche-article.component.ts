f649bc9a2862a31777f00836c5593df695730495
Thomas Ayissi <thomas8sea@gmail.com>
2023-01-09 15:57:22 +0100

    Modifications et problèmes d'affichage d'images https://www.journaldunet.fr/web-tech/developpement/1441265-comment-charger-une-image-sous-angular/ Solution essayée, mais ne semble pas efficace. TO DO : -> identifier le chemin relatif correct permettant d'atteindre les images afin qu'elles s'affichent

--- src/app/fiche-article/fiche-article.component.ts
+++ src/app/fiche-article/fiche-article.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./fiche-article.component.scss']
 })
 export class FicheArticleComponent {
+   category!: string;
+   title!: string;
+   isbn_number!: string;
+   publication_date!: Date;
+   author!: string;
+   resume!: string;
+   image_url!:string;
+
+   ngOnInit(){
+    this.category= 'Thriller';
+    this.title= 'An incredible story';
+    this.isbn_number= '123 456 789 101';
+    this.publication_date= new Date();
+    this.author= 'Bruce Wayne';
+    this.resume= 'Amazing book with an interessting story';
+    this.image_url= "/assets/imagesFolder/Livre_barack_obama_une_terre_promise.jpeg";
+
+  }
 
 }
+
+
