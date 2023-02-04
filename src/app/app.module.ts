32e4d267fdf04021c02b2d9736442e38ae5e93a0
Thomas Ayissi <thomas8sea@gmail.com>
2023-02-04 18:21:17 +0100

    suite retrait des références aux deux modules déplacés

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FicheArticleComponent } from './fiche-article/fiche-article.component';
-import { BoutonMenuComponent } from './bouton-menu/bouton-menu.component';
 
 @NgModule({
   declarations: [
